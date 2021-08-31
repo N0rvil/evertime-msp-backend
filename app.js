9b85d1e1c0c568a6866c8787d3158617b187777b
hlasensky <tomas.hlasensky@seznam.cz>
2021-08-31 12:48:40 +0200

    changing models(adding username)

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const express = require('express');
+const express = require("express");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
-const cors = require('cors');
+const cors = require("cors");
 
-const mongoose = require('mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
-require('dotenv').config()
+const mongoose = require("mongoose");
+const User = require("./models/user");
+const Task = require("./models/task");
+require("dotenv").config();
 
 const PORT = 3005;
 MONGOSE_URI = process.env.MONGO_URI;
-
 
 let listOfOnGoingTasks = [];
 let listOfDoneOrFaildTasks = [];
@@ -19,9 +18,14 @@ const app = express();
 const store = new MongoDBStore({
 	uri: MONGOSE_URI,
 	collection: "sessions",
-})
+});
 
-app.use(cors());
+let corsOptions = {
+	origin: ["http://localhost:3000", MONGOSE_URI],
+	credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -29,24 +33,115 @@ app.use(
 		secret: "my secret",
 		resave: false,
 		saveUninitialized: false,
-		store: store
+		store: store,
 	})
 );
 
 app.use((req, res, next) => {
-    console.log(req.session.user)
 	if (!req.session.user) {
 		return next();
-	};
-    User.findById(req.session.user._id)
+	}
+	User.findById(req.session.user._id)
 		.then((user) => {
-            req.user = user;
-            console.log(req.user)
-            console.log(req.session.user)
-			next()
-    }).catch((err) => console.log(err));
+			req.user = user;
+			next();
+		})
+		.catch((err) => console.log(err));
 });
 
+app.post("/signin", async (req, res) => {
+	const username = req.body.username;
+	const email = req.body.email;
+	const password = req.body.password;
+	// from frontend is passed username and password
+	User.findOne({ email: email })
+		// if user doesnt exist it will create new one
+		.then((user) => {
+			if (user === null) {
+				User.create(
+					{
+						username: username,
+						email: email,
+						password: password,
+						tasks: { tasks: [] },
+					},
+					(err) => {
+						if (err) console.log(err);
+					}
+				);
+				res.json({ note: "created" });
+			} else {
+				// if user password doesnt match it will response wrong password else it will loged the user
+				if (user.password === req.body.password) {
+					res.json({ note: "loged" });
+				} else {
+					res.json({ note: "wrong password" });
+				}
+			}
+		});
+});
+
+app.post("/add-task", async (req, res, next) => {
+	listOfOnGoingTasks = [];
+	listOfDoneOrFaildTasks = [];
+	const description = req.body.description;
+	const duration = req.body.duration;
+	const user_id = "612d2352062cf0331cf296b2";
+	const task = new Task({
+		description: description,
+		duration: new Date().getTime() + duration * 24 * 3600000,
+		done: false,
+		userId: user_id,
+	});
+	task.save()
+		.then(async () => {
+			const tasks = await Task.find({
+				userId: user_id,
+			});
+			tasks.forEach((task) => {
+				if (
+					task.done === true ||
+					task.duration < new Date().getTime()
+				) {
+					listOfDoneOrFaildTasks.push(task);
+				} else {
+					listOfOnGoingTasks.push(task);
+				}
+			});
+			res.json({ listOfOnGoingTasks, listOfDoneOrFaildTasks });
+		})
+		.catch((err) => console.log(err));
+});
+
+app.post("/fetch-tasks", async (req, res, next) => {
+	listOfOnGoingTasks = [];
+	listOfDoneOrFaildTasks = [];
+	const user_id = "612d2352062cf0331cf296b2";
+	const tasks = await Task.find({ userId: user_id });
+	tasks.forEach((task) => {
+		if (task.done === true || task.duration < new Date().getTime()) {
+			listOfDoneOrFaildTasks.push(task);
+		} else {
+			listOfOnGoingTasks.push(task);
+		}
+	});
+	res.json({ listOfOnGoingTasks, listOfDoneOrFaildTasks });
+});
+
+app.post("/done", async (req, res, next) => {
+	const idOfTaskToChange = req.body.done;
+	console.log(req.body.done);
+	Task.findById(idOfTaskToChange)
+		.then((task) => {
+			task.done = !task.done;
+			return task.save();
+		})
+		.then((result) => {
+			console.log("changed");
+			res.json({ note: "Good job!" });
+		})
+		.catch((err) => console.log(err));
+});
 
 mongoose
 	.connect(MONGOSE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -56,95 +151,3 @@ mongoose
 	.catch((err) => {
 		console.log(err);
 	});
-
-app.post('/signin', async (req, res) => {
-    // from frontend is passed username and password
-    User.findOne({email: req.body.username})
-    // if user doesnt exist it will create new one
-        .then(user => {
-            req.session.user = user;
-            console.log(req.user)
-            if (user === null) {
-                User.create({ 
-                    email: req.body.username,
-                    password: req.body.password,
-                    tasks: []
-                }, (err) => {
-                if (err) console.log(err);
-                });
-                res.json({ note: 'created' });
-            } else {
-                // if user password doesnt match it will response wrong password else it will loged the user
-                if (user.password === req.body.password) {
-                    res.json({ note: 'loged' })
-
-                } else {
-                    res.json({ note: 'wrong password' })
-                }   
-            }        
-    })
-
-});
-
-app.post("/add-task", async (req, res, next) => {
-    listOfOnGoingTasks = [];
-    listOfDoneOrFaildTasks = [];
-    const description = req.body.description;
-    const duration = req.body.duration;
-    console.log(description, duration)
-    const task = new Task({
-        description: description,
-        duration: new Date().getTime() + (duration * 24 * 3600000),
-        done: false,
-        userId: "61253b20f83af946946bdfd0"
-    })
-    task
-        .save()
-        .then(async () => {
-            const tasks = await Task.find({ userId: "61253b20f83af946946bdfd0" });
-            tasks.forEach(task => {
-                if (task.done === true || task.duration < new Date().getTime()) {
-                    console.log("done or expired");
-                    listOfDoneOrFaildTasks.push(task)
-                } else {
-                    console.log("not done tasks or nonexpored");
-                    listOfOnGoingTasks.push(task)
-                }
-                
-            })
-            res.json({ listOfOnGoingTasks, listOfDoneOrFaildTasks });
-        })
-        .catch(err => console.log(err))
-});
-
-app.post("/fetch-tasks", async (req, res, next) => {
-    listOfOnGoingTasks = [];
-    listOfDoneOrFaildTasks = [];
-    const user = req.session.user;
-    const tasks = await Task.find( {userId: "61253b20f83af946946bdfd0"} );
-    tasks.forEach(task => {
-        if (task.done === true || task.duration < new Date().getTime()) {
-            console.log("done or expired");
-            listOfDoneOrFaildTasks.push(task)
-        } else {
-            console.log("not done tasks or nonexpored");
-            listOfOnGoingTasks.push(task)
-        }
-        
-    })
-    res.json({ listOfOnGoingTasks, listOfDoneOrFaildTasks });
-});
-
-app.post("/done", async (req, res, next) => {
-    const idOfTaskToChange = req.body.done;
-    console.log(req.body.done)
-    Task.findById(idOfTaskToChange)
-    .then(task => {
-        task.done = !task.done;
-        return task.save();
-    })
-    .then(result => {
-        console.log("changed")
-        res.json({note: "Good job!"})
-    }).catch(err => console.log(err));
-});
--- models/user.js
+++ models/user.js
@@ -1,29 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const user = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  tasks: [
-    {
-      task: {
-        type: Schema.Types.ObjectId,
-        ref: 'Task',
-        required: true
-      },
-      done: { type: Boolean, required: true }
-      }
-    
-  ]
-  
-  });
+	username: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	score: {
+		type: Number,
+		required: false,
+	},
+	checkBoard: {
+		tasks: [
+			{
+				task: {
+					type: Schema.Types.ObjectId,
+					ref: "Task",
+					required: true,
+				},
+				done: { type: Boolean, required: true }
+			},
+		],
+	},
+});
 
-const User = mongoose.model('User', user);
+const User = mongoose.model("User", user);
 
 module.exports = User;
